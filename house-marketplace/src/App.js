ad94be32e21c7d1b22045b6c134a02a2a9821df4
Alex Elmore <aelmore@mrk-c02gf1f1md6r.lan>
2022-02-08 13:58:23 -0600

    Create page and navbar components and setup router and routes

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Explore />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/profile" element={<SignIn />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
