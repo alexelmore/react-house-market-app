93fffb52b6047a2f55687508d8325c177ecb0f0d
Alex <aelmore@merkleinc.com>
2022-02-16 09:54:21 -0600

    Start setup for profile page

--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,7 +1,17 @@
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+
 function Profile() {
+  const auth = getAuth();
+  console.log(auth);
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+
+  const [user, setUser] = useState(null);
   return (
     <div>
-      <h1>Profile Page</h1>
+      {user ? <h1>Greetings {user.displayName}</h1> : <h1>Not Logged In</h1>}
     </div>
   );
 }
