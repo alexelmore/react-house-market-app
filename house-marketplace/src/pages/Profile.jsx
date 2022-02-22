0c8c6c0c30453d82c188021f0641361a9b0e6fee
Alex <aelmore@merkleinc.com>
2022-02-22 10:02:09 -0600

    Start setup for profile page and logout

--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,17 +1,37 @@
 import { getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
   const auth = getAuth();
-  console.log(auth);
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  const [user, setUser] = useState(null);
+  // Destructure and pull name and email out of formData
+  const { name, email } = formData;
+
+  // Init useNavigate hook
+  const navigate = useNavigate();
+
+  // Function to log the user out
+  const onLogout = (e) => {
+    auth.signOut();
+    navigate("/");
+  };
   return (
-    <div>
-      {user ? <h1>Greetings {user.displayName}</h1> : <h1>Not Logged In</h1>}
+    <div className="profile">
+      {formData ? (
+        <header className="profileHeader">
+          <p className="pageHeader">My Profile</p>
+          <button className="logOut" type="button" onClick={onLogout}>
+            Logout
+          </button>
+        </header>
+      ) : (
+        <h1>Not Logged In</h1>
+      )}
     </div>
   );
 }
