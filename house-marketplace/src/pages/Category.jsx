5ce546cae2172e46f196dc72ae44a72023568c39
Alex <aelmore@merkleinc.com>
2022-02-23 08:57:57 -0600

    setup for Category Component

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -1,5 +1,123 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
 function Category() {
-  return <div>Category Page</div>;
+  // Component level state
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+  // Init params
+  const params = useParams();
+
+  // useEffect hook to make call to firebase for listings
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, "listings");
+
+        // Create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch listings");
+      }
+    };
+
+    fetchListings();
+  }, [params.categoryName]);
+
+  // Setup for listings pagination
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, "listings");
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where("type", "==", params.categoryName),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not fetch listings");
+    }
+  };
+  console.log(listings);
+  return (
+    <div className="category">
+      <header className="pageHeader">
+        {params.categoryName === "rent" ? "Places for rent" : "Places for sale"}
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <main>
+          <ul className="categoryListings">
+            {listings.map((listing) => (
+              <h3 key={listing.id}>{listing.data.name}</h3>
+            ))}
+          </ul>
+        </main>
+      ) : (
+        <p>No listings {params.categoryName}</p>
+      )}
+    </div>
+  );
 }
 
 export default Category;
