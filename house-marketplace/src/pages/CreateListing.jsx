a21445108cee19de6211380a2446be20f2eaa3cc
Alex <aelmore@merkleinc.com>
2022-02-25 08:36:21 -0600

    Further setup for geolocation

--- house-marketplace/src/pages/CreateListing.jsx
+++ house-marketplace/src/pages/CreateListing.jsx
@@ -100,14 +100,25 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAxZuO2FCLMZu20ZDLCbSW4zEPyJQnQ8lA`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       );
       const data = await response.json();
-      console.log(data);
+      const { results } = data;
+      geolocation.lat = results[0].geometry.location.lat ?? 0;
+      geolocation.lng = results[0].geometry.location.lng ?? 0;
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : results[0]?.formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
     setLoading(false);
   };
