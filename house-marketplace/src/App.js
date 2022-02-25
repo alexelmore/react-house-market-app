ca7c791a42bfb590639521ed29a221021f51d108
Alex <aelmore@merkleinc.com>
2022-02-25 13:52:29 -0600

    Inital setup for Listing Component

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -12,6 +12,8 @@ import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+
 function App() {
   return (
     <>
@@ -27,6 +29,10 @@ function App() {
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          ></Route>
           <Route path="/create-listing" element={<CreateListing />}></Route>
 
           <Route path="/*" element={<NotFound />}></Route>
