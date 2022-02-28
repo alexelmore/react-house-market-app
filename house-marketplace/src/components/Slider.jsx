b81956422890a86cf556f73d850f1eef6a969c3f
Alex <aelmore@merkleinc.com>
2022-02-28 08:05:14 -0600

    Setup for Slider Component

---
+++ house-marketplace/src/components/Slider.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { db } from "../firebase.config";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import Spinner from "./Spinner";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+function Slider() {
+  // Component Level State
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  // Init useNavigate Hook
+  const navigate = useNavigate();
+
+  // useEffect Hook that gets user and listings on page load
+  useEffect(() => {
+    // Fetch the listings
+    const fetchListings = async () => {
+      // Setup the listings, query and db snapshot
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+      // listings variable set to an empty array
+      let listings = [];
+      // loop through querySnap and push results to listings variable
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      // Re-set listings and loading State
+      setListings(listings);
+      setLoading(false);
+    };
+    // Call function to fetch the listings on page load
+    fetchListings();
+  }, []);
+  // If loading is true, display Spinner Component
+  if (loading) {
+    return <Spinner />;
+  }
+  // If no listings, just return
+  if (listings.length === 0) {
+    return <></>;
+  }
+  return (
+    listings && (
+      <>
+        <p className="exploreHeading">Recommended</p>
+
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  background: `url(${data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="swiperSlideDiv"
+              >
+                <p className="swiperSlideText">{data.name}</p>
+                <p className="swiperSlidePrice">
+                  ${data.discountedPrice ?? data.regularPrice}{" "}
+                  {data.type === "rent" && "/ month"}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
+}
+
+export default Slider;
